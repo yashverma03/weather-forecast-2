3d65a009ad40cb20d6ac541e2e46bf7627415a7c
yasherma03 <vermayash2003@gmail.com>
2024-09-21 12:35:16 +0530

    feat: initial setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import Home from './pages/Home/Home';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { queryClient } from './config/query-client';
+import Home from './pages/Home/Home';
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
